ae566ae0337032afa5fe0146380ef9f7993b2946
ankandasim4 <ankanim4@gmail.com>
2024-05-22 20:52:02 +0530

    Initial commit v1

--- web/app/application/config/config.php
+++ web/app/application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'https://master-7rqtwti-2tyvb2tqirnhg.us.platformsh.site/';
+$config['base_url'] = '';
 
 /*
 |--------------------------------------------------------------------------
